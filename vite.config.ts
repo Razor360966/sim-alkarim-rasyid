5fc26f3991d45a48d83dc65bee0c12f05b6bf992
Rahman Azizi <razor6155@gmail.com>
2026-07-30 21:32:14 +0700

    menambahkan PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,46 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+  react(),
+  tailwindcss(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+    maximumFileSizeToCacheInBytes: 10 * 1024 * 1024
+},
+    includeAssets: [
+      'favicon.ico',
+      'apple-touch-icon.png'
+    ],
+    manifest: {
+      name: 'SIMAK SMP Alkarim',
+      short_name: 'SIMAK',
+      description: 'Sistem Manajemen Akademik SMP Alkarim Rasyid',
+      theme_color: '#0f766e',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait',
+      start_url: '/',
+      icons: [
+        {
+          src: 'icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
